c5c5f48cfdc30667d44ab8746f4990d71598d8f1
Pavel Afanasev <afanasyevpv9@gmail.com>
2024-03-19 15:53:52 +0300

    Initial Commit

---
+++ WeatherForecastForVK/WeatherForecastForVKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastForVKApp.swift
+//  WeatherForecastForVK
+//
+//  Created by Pavel Afanasev on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastForVKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
